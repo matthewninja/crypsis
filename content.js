8b7156a7be08a70140a0cfffa4b6de9534e8a1bf
matthewninja <matthewham4@gmail.com>
2019-08-24 03:05:35 -0400

    Added MobileNet model

--- content.js
+++ content.js
@@ -2,6 +2,11 @@ let tensor_flow = document.createElement('script');
 tensor_flow.src = 'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.0.0/dist/tf.min.js';
 document.getElementsByTagName("head")[0].appendChild(tensor_flow);
 
+// Load the MobileNet model.
+let model = document.createElement('script');
+model.src = 'https://cdn.jsdelivr.net/npm/@tensorflow-models/mobilenet@1.0.0';
+document.getElementsByTagName("head")[0].appendChild(model);
+
 // get all of the images on the page
 console.log("src list of images:");
 let images = document.getElementsByTagName('img'); 
