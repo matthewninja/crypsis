bbc7bbe51f0e1f53bafce94c04c0288ba72bbe5d
matthewninja <matthewham4@gmail.com>
2019-08-24 02:45:16 -0400

    Added tensorflow to script

--- content.js
+++ content.js
@@ -1,7 +1,12 @@
+let tensor_flow = document.createElement('script');
+tensor_flow.src = 'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.0.0/dist/tf.min.js';
+document.getElementsByTagName("head")[0].appendChild(tensor_flow);
+
+// get all of the images on the page
 console.log("src list of images:");
-var images = document.getElementsByTagName('img'); 
-var srcList = [];
-for(var i = 0; i < images.length; i++) {
+let images = document.getElementsByTagName('img'); 
+let srcList = [];
+for(let i = 0; i < images.length; i++) {
     srcList.push(images[i].src);
 }
 console.log(srcList);
