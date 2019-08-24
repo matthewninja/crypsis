ee4666bb6ffeeb60ee493a6dc796136d8500ba9c
ericrabiner <44846748+ericrabiner@users.noreply.github.com>
2019-08-24 11:50:47 -0400

    First classification

--- js/content.js
+++ js/content.js
@@ -1,39 +1,25 @@
-let tensor_flow = document.createElement('script');
-tensor_flow.src = 'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.0.0/dist/tf.min.js';
-document.getElementsByTagName("head")[0].appendChild(tensor_flow);
-
-// Load the MobileNet model.
-let model = document.createElement('script');
-model.src = 'https://cdn.jsdelivr.net/npm/@tensorflow-models/mobilenet@1.0.0';
-document.getElementsByTagName("head")[0].appendChild(model);
-
-// get all of the images on the page
-console.log("src list of images:");
-let images = document.getElementsByTagName('img'); 
-let srcList = [];
-for(let i = 0; i < images.length; i++) {
-    srcList.push(images[i].src);
-}
-// console.log(srcList);
-const img = document.getElementById('img');
-console.log("HEllo")
-
 function getImages() {
   return new Promise((resolve, reject) => {
-    resolve("Hello");
-
+    let images = $("img");
+    let srcList = [];
+    for (let i = 0; i < images.length; i++) {
+      srcList.push(images[i].src);
+    }
+    resolve(images);
   })
 }
 
-getImages().then((msg) => {
-  console.log(msg)
+getImages().then((images) => {
+  console.log("src list of images:");
+  console.log(images)
+  // Load the model.
+  mobilenet.load().then(model => {
+    // Classify the image.
+    model.classify(images[0]).then(predictions => {
+      console.log('Predictions: ');
+      console.log(predictions);
+    });
+  });
 })
 
-// Load the model.
-// mobilenet.load().then(model => {
-//   // Classify the image.
-//   model.classify(img).then(predictions => {
-//     console.log('Predictions: ');
-//     console.log(predictions);
-//   });
-// });
+
