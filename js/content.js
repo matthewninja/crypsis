c8ae97e4a0aa0cf9b8ee352d8c787c2154ca921a
ericrabiner <44846748+ericrabiner@users.noreply.github.com>
2019-08-24 12:07:54 -0400

    match case: cat with prob > 50%, switch to dog image

--- js/content.js
+++ js/content.js
@@ -1,25 +1,26 @@
 function getImages() {
   return new Promise((resolve, reject) => {
     let images = $("img");
-    let srcList = [];
-    for (let i = 0; i < images.length; i++) {
-      srcList.push(images[i].src);
-    }
     resolve(images);
   })
 }
 
-getImages().then((images) => {
+getImages().then(images => {
   console.log("src list of images:");
   console.log(images)
   // Load the model.
   mobilenet.load().then(model => {
-    // Classify the image.
-    model.classify(images[0]).then(predictions => {
-      console.log('Predictions: ');
-      console.log(predictions);
-    });
+    // Classify each image.
+    for (let i = 0; i < images.length; i++) {
+      model.classify(images[i]).then(predictions => {
+        console.log('Predictions: ');
+        console.log(predictions);
+        for (let i = 0; i < predictions.length; i++) {
+          if (predictions[i].className.match(/cat/g) && predictions[i].probability >= 0.50) {
+            images[i].src = "./images/dog.jpg";
+          }
+        }
+      });
+    }
   });
 })
-
-
